6f8410d9262023bab0370658833b723b9934f8be
Ortster <kurnath@gmail.com>
2025-01-29 20:48:05 -0600

    restore Rag character folder for ease of future updating

---
+++ Rag_5040/RNG.lua
@@ -0,0 +1,180 @@
+local profile = {}
+
+local fastCastValue = 0.00 -- 0% from gear
+
+local special_ammo = "Carapace Bullet"
+
+local sets = {
+    Idle = {},
+    IdleALT = {},
+    Resting = {},
+    Town = {},
+    Movement = {},
+
+    DT = {},
+    MDT = { -- Shell IV provides 23% MDT
+    },
+    FireRes = {},
+    IceRes = {},
+    LightningRes = {},
+    EarthRes = {},
+    WindRes = {},
+    WaterRes = {},
+    Evasion = {},
+
+    Precast = {},
+    SIRD = {
+    },
+    Haste = { -- Used for Utsusemi cooldown
+    },
+
+    LockSet1 = {},
+    LockSet2 = {},
+    LockSet3 = {},
+
+    Preshot = {},
+
+    TP_LowAcc = {},
+    TP_HighAcc = {},
+
+    Ranged_ACC = {},
+    Ranged_ATK = {},
+
+    EnmityDown = {},
+
+    Barrage = {},
+    Scavenge = {},
+    Shadowbind = {},
+    Camouflage = {},
+    Sharpshot = {},
+    UnlimitedShot = {},
+
+    WS = {},
+    WS_SlugShot = {},
+    WS_Coronach = {},
+}
+profile.Sets = sets
+
+profile.SetMacroBook = function()
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro book 1')
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1')
+end
+
+--[[
+--------------------------------
+Everything below can be ignored.
+--------------------------------
+]]
+
+gcmelee = gFunc.LoadFile('common\\gcmelee.lua')
+
+profile.HandleAbility = function()
+    gFunc.EquipSet(sets.EnmityDown)
+
+    local action = gData.GetAction()
+    if (action.Name == 'Scavenge') then
+        gFunc.EquipSet(sets.Scavenge)
+    elseif (action.Name == 'Shadowbind') then
+        gFunc.EquipSet(sets.Shadowbind)
+    elseif (action.Name == 'Camouflage') then
+        gFunc.EquipSet(sets.Camouflage)
+    elseif (action.Name == 'Sharpshot') then
+        gFunc.EquipSet(sets.Sharpshot)
+    elseif (action.Name == 'Eagle Eye Shot') then
+        gFunc.EquipSet(sets.Ranged_ATK)
+
+        local equipment = gData.GetEquipment()
+        local unlimitedShot = gData.GetBuffCount('Unlimited Shot')
+        if (unlimitedShot == 1) then
+            gFunc.EquipSet(sets.UnlimitedShot)
+        elseif (equipment.Ammo ~= nil and equipment.Ammo.Name == special_ammo) then
+            print(chat.header('RNG'):append(chat.message('Action Canceled: Special Ammo Protection')))
+            gFunc.CancelAction()
+        end
+    end
+end
+
+profile.HandleItem = function()
+    gcinclude.DoItem()
+end
+
+profile.HandlePreshot = function()
+    gFunc.EquipSet(sets.Preshot)
+
+    local equipment = gData.GetEquipment()
+    local unlimitedShot = gData.GetBuffCount('Unlimited Shot')
+    if (unlimitedShot == 1) then
+        gFunc.EquipSet(sets.UnlimitedShot)
+    end
+    if (unlimitedShot == 0 and equipment.Ammo ~= nil and equipment.Ammo.Name == special_ammo) then
+        print(chat.header('RNG'):append(chat.message('Action Canceled: Special Ammo Protection')))
+        gFunc.CancelAction()
+    end
+end
+
+profile.HandleMidshot = function()
+    gFunc.EquipSet(sets.Ranged_ACC)
+
+    local barrage = gData.GetBuffCount('Barrage')
+    if (barrage == 1) then
+        gFunc.EquipSet(sets.Barrage)
+    end
+
+    local unlimitedShot = gData.GetBuffCount('Unlimited Shot')
+    if (unlimitedShot == 1) then
+        gFunc.EquipSet(sets.UnlimitedShot)
+    end
+end
+
+profile.HandleWeaponskill = function()
+    gFunc.EquipSet(sets.WS)
+
+    local equipment = gData.GetEquipment()
+    local unlimitedShot = gData.GetBuffCount('Unlimited Shot')
+    if (unlimitedShot == 1) then
+        gFunc.EquipSet(sets.UnlimitedShot)
+    elseif (equipment.Ammo ~= nil and equipment.Ammo.Name == special_ammo) then
+        print(chat.header('RNG'):append(chat.message('Action Canceled: Special Ammo Protection')))
+        gFunc.CancelAction()
+    end
+
+    local action = gData.GetAction()
+    if (action.Name == 'Slug Shot') then
+        gFunc.EquipSet(sets.WS_SlugShot)
+    elseif (action.Name == 'Coronach') then
+        gFunc.EquipSet(sets.WS_Coronach)
+    end
+end
+
+profile.OnLoad = function()
+    gcmelee.Load()
+    profile.SetMacroBook()
+end
+
+profile.OnUnload = function()
+    gcmelee.Unload()
+end
+
+profile.HandleCommand = function(args)
+    gcmelee.DoCommands(args)
+
+    if (args[1] == 'horizonmode') then
+        profile.HandleDefault()
+    end
+end
+
+profile.HandleDefault = function()
+    gcmelee.DoDefault()
+    gcmelee.DoDefaultOverride()
+    gFunc.EquipSet(gcinclude.BuildLockableSet(gData.GetEquipment()))
+end
+
+profile.HandlePrecast = function()
+    gcmelee.DoPrecast(fastCastValue)
+end
+
+profile.HandleMidcast = function()
+    gcmelee.DoMidcast(sets)
+end
+
+return profile
